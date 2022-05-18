594aac31054a0f831591808b4810623978396f69
Leonel Guerrero <85099589+LemonMantis5571@users.noreply.github.com>
2022-05-17 22:03:32 -0600

    first commit

---
+++ Java Secuencial/JavaScript Educative/turca.js
@@ -0,0 +1,10 @@
+// Write your code here
+var turca = false; 
+for (var i = false;  i <= turca; i++){
+    even = parseInt(prompt("Enter your number: "));
+    if (even <= 50) {
+        i = true;
+    }
+    
+}
+
